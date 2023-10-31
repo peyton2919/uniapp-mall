8448714dca150ff447d0b0676f4ad67f435e3b38
peyton2919 <fz2919@tom.com>
2023-10-31 13:09:09 +0800

    update order 订单

--- pages/order/order.vue
+++ pages/order/order.vue
@@ -1,22 +1,67 @@
 <template>
 	<!-- 订单 -->
+	<!-- 5.1.1 搜索列表页面
+		1. 点击 底部导航栏 我的; 
+		2. 跳转到 mine 页面, 点击 全部订单;
+		3. 跳转到 本页面{order};
+		 -->
 	<view style="background: #f5f5f5;">
 		
 		<!-- <view class="position-relative" style="height: 400rpx;">
 			<no-thing icon="no_pay"></no-thing>
 		</view> -->
 		<!-- 选项卡 -->
-		<view class="d-flex bg-white a-center font-md text-muted border-top ">
-			<view class="flex-1 d-flex a-center j-center py-2"
+		<view class="d-flex bg-white a-center font-md text-muted border-top border-light-secondary">
+			<view class="flex-1 d-flex a-center j-center py-2 "
 				v-for="(item,index) in tabBars" :key="index"
-				:class="index === tabIndex ? ' tab_active' : ''"
+				:class="index === tabIndex ? ' tab_active' : ' border-bottom'"
 				@tap="changeTab(index)">
 				{{item.name}}
 			</view>
 		</view>
 		<!-- 订单列表 -->
 		<view class="position-relative">
-			{{tabBars[tabIndex].name}}
+			<block v-for="(item,index) in list" :key="index">
+				<view class="bg-white">
+					<!-- 头部 -->
+					<view class="d-flex a-center p-2 border-bottom border-light-secondary">
+						<text class="text-light-muted font-md">2023-10-25 12:00</text>
+						<text class="main-text-color ml-auto font-md">已发货</text>
+					</view>
+					<!-- 主体 -->
+					<view class="px-2">
+						<view class="border-bottom d-flex a-center py-2 border-light-secondary">
+							<image src="/static/images/demo/demo6.jpg" mode="widthFix" style="width: 150rpx; height: 150rpx;"
+								class="rounded mx-2 flex-shrink"></image>
+							<view class="flex-1">
+								<view class="d-flex a-center">
+									<text class="font-md text-dark">小米8</text>
+									<text class="font-md text-light-muted ml-auto">￥1999.00</text>
+								</view>
+								<view class="d-flex a-center">
+									<text class="font text-light-muted">金色</text>
+									<text class="font text-light-muted ml-auto">x 1</text>
+								</view>
+							</view>
+						</view>
+					</view>
+					<!-- 底部 -->
+					<view class="d-flex a-center py-2 px-3">
+						<text class="text-dark font-md ml-auto">共3件商品,合计: ￥299.00</text>
+						
+					</view>
+					<view class="d-flex j-end pb-2">
+						<view class="rounded border border-light-secondary px-3 py-1 text-secondary" 
+							hover-class="bg-light-secondary">
+							查看物流
+						</view>
+						<view class="rounded border border-light-secondary px-3 py-1 mx-1 text-secondary"
+							hover-class="bg-light-secondary">
+							确认收货
+						</view>
+					</view>
+				</view>
+			</block>
 		</view>
 		
 		<!-- 为你推荐 -->
@@ -87,6 +132,29 @@
 						pprice: 1366.00
 					}
 				],
+				list: [
+					{
+						createTime:'2023-10-25 12:00',
+						status: '已发货',
+						order_items: [
+							{
+								cover: '/static/images/demo/demo6.jpg',
+								title: '小米8',
+								pprice: '1999.00',
+								attrs: '金色',
+								num: 1
+							},{
+								cover: '/static/images/demo/demo6.jpg',
+								title: '小米8',
+								pprice: '1999.00',
+								attrs: '金色',
+								num: 1
+							}
+						],
+						totalNum: 3,
+						totalPrice: 299
+					}
+				]
 			}
 		},
 		methods: {
@@ -100,8 +168,8 @@
 
 <style>
 	.tab_active {
-		border-bottom: 5rpx solid #fd6801;
-		color: #fd6801;
-		margin-bottom: -5rpx;
+		border-bottom: 8rpx solid #fd6801!important;
+		color: #fd6801!important;
+		/* margin-bottom: -5rpx; */
 	}
 </style>
